e24065595be0c6b11841ca93d9ac51cf03319a79
dvru <damvaru4@gmail.com>
2020-05-26 14:34:47 -0500

    changes made to app

--- plants-made-easy/src/App.js
+++ plants-made-easy/src/App.js
@@ -2,36 +2,30 @@ import React from 'react';
 // import logo from './logo.svg';
 import './App.css';
 
+
 class App extends React.Component { 
 
+  
   constructor() {
     super();
     
     this.state = {
-      plants: [
-        {
-          name: 'Primrose',
-          id: 'pl1'
-        },
-        {
-          name: 'Poppies',
-          id: 'pl2'
-        },
-        {
-          name: 'Lavender',
-          id: 'pl3'
-        },
-      ]
+      plants: []
     };
   }
-
+  
+  componentDidMount(){
+    fetch("https://jsonplaceholder.typicode.com/users")
+    .then(response => response.json())
+    .then(plants => this.setState({ plants: plants}));
+  }
 
   render(){ // using jsx syntax
     return (
       <div className="App">
        {
          this.state.plants.map(plant => (
-           <h1 key={plant.id}> {plant.name} </h1>
+            <h1 key={plant.id}> {plant.name} </h1>
          ))}
       </div>
     )

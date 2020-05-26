c6a001b6a51047829f34f1c9a38c9d98eaf362d3
dvru <damvaru4@gmail.com>
2020-05-26 12:34:14 -0500

    changes made to app

--- plants-made-easy/src/App.js
+++ plants-made-easy/src/App.js
@@ -2,7 +2,7 @@ import React, {Component} from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-class App extends Component {
+class App extends Component { 
 
   constructor() {
     super();
@@ -13,7 +13,7 @@ class App extends Component {
   }
 
 
-  render(){
+  render(){ // using jsx syntax
     return (
       <div className="App">
         <header className="App-header">

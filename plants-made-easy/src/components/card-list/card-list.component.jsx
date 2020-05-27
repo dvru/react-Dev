0f4b80f117aa0f4625c659e2679071a508d2b93c
dvru <damvaru4@gmail.com>
2020-05-27 17:29:13 -0500

    added card style css

--- plants-made-easy/src/components/card-list/card-list.component.jsx
+++ plants-made-easy/src/components/card-list/card-list.component.jsx
@@ -1,10 +1,11 @@
 import React from 'react';
+import { Card } from '../card/card.component';
 import './card-list.styles.css';
 
 export const Cardlist = props => (
     <div className='card-list'>
          {props.plants.map(plant => (
-            <h1 key={plant.id}> {plant.name} </h1>
+            <Card key={plant.id} plant={plant}/>
          ))}
     </div>
 ) 
---
+++ plants-made-easy/src/components/card/card.component.jsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+export const Card = (props) => (
+    <div>
+        <h1> {props.plant.name} </h1>
+    </div>
+)

3727d734103c86845da07cf3bf38a8de7bf3051e
dvru <damvaru4@gmail.com>
2020-05-27 20:33:04 -0500

    added to card

--- plants-made-easy/src/components/card-list/card-list.component.jsx
+++ plants-made-easy/src/components/card-list/card-list.component.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import { Card } from '../card/card.component';
 import './card-list.styles.css';
 
+// list of cards
 export const Cardlist = props => (
     <div className='card-list'>
          {props.plants.map(plant => (

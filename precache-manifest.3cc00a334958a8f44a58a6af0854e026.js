fb326241f05d241c9164ab2cdfc824091a633b6e
dvru <damvaru4@gmail.com>
2020-05-28 16:07:23 -0500

    Updates

---
+++ precache-manifest.3cc00a334958a8f44a58a6af0854e026.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "12e17ea4befb67d6ce74b4d1d85778d7",
+    "url": "/plants-made-easy/index.html"
+  },
+  {
+    "revision": "2be4d3bb26d6990334c7",
+    "url": "/plants-made-easy/static/css/main.51fe0173.chunk.css"
+  },
+  {
+    "revision": "5ea6b682007f425a560a",
+    "url": "/plants-made-easy/static/js/2.5927a73e.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/plants-made-easy/static/js/2.5927a73e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2be4d3bb26d6990334c7",
+    "url": "/plants-made-easy/static/js/main.9fd76a1e.chunk.js"
+  },
+  {
+    "revision": "72e66c93cd40471092fe",
+    "url": "/plants-made-easy/static/js/runtime-main.da344ab5.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.6faaa5493426eb85a5b485b7698ef8d7.js"
+  "/plants-made-easy/precache-manifest.3cc00a334958a8f44a58a6af0854e026.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/plants-made-easy/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
